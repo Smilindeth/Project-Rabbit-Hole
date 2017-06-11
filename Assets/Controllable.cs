cceef2bcc94b7ce039f997eeef13d966481e51fd
Smilindeth <damaster3001@yahoo.com>
2017-06-10 21:09:13 -0600

    Input start

--- Assets/Controllable.cs
+++ Assets/Controllable.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Controllable : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +15,65 @@ public class Controllable : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private class PlayerControls
+    {
+        public int Player { get; set; }
+        public Dictionary<Controls, List<InputType>> Inputs { get; set; } = new Dictionary<Controls, List<InputType>>();
+
+        public abstract class InputType
+        {
+            public virtual bool Active { get; }
+            public virtual float Value { get; }
+            public virtual string Name { get; set; }
+        }
+
+        public class KeyboardInput : InputType
+        {
+            private KeyCode assignedButton;
+            private string customButtonName = null;
+            public override bool Active => Input.GetKeyDown(assignedButton);
+            public override float Value => 1f;
+            public override string Name
+            {
+                get
+                {
+                    return customButtonName ?? assignedButton.ToString();
+                }
+                set
+                {
+                    customButtonName = value;
+                }
+            }
+        }
+    }
+}
+
+public enum Controls
+{
+    Up,
+    Down,
+    Left,
+    Right,
+    Forward,
+    Backward,
+    Jump,
+    Pause,
+    Inventory,
+    Map,
+    Confirm,
+    Cancel,
+    LightAttack,
+    HeavyAttack,
+    Aim,
+    Fire,
+    WeaponSelect,
+    Defend,
+    Crouch,
+    Dash,
+    Dodge,
+    Lockon,
+    Glide,
+
+    // Change as needed
 }
